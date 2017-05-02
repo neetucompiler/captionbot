e660784e8f4534830cb9c3a139f87305111aba4f
Neetu  Mishra <1191193@India.TCS.com>
2017-05-02 11:51:37 +0530

    one more time

--- captionservice.js
+++ captionservice.js
@@ -49,7 +49,7 @@ module.exports.getCaptionFromUrl = function (myurl) {
                 'content-type': 'application/json',
                 'Ocp-Apim-Subscription-Key': '1ab0cd9b2fb94de4b84b9a14fd7f8c0f'
                 },
-                json: JSON.stringify({ url: myurl })
+                text: myurl
             };
 
             request.post(requestData, function (error, response, body) {
